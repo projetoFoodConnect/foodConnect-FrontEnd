f0f11935d277b99f58c157a19ca3ad079c0ab903
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-02 09:56:53 -0300

    fix: susbtitui tailwind antigo por versao com vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './global.css'
+import './index.css'
 import { AuthProvider } from './app/features/auth/contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
