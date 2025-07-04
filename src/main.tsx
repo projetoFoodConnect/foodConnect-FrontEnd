8b6f09229866805da1bb6d3fe05690543d3611f5
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-04 08:37:08 -0300

    feat: alerts bonitinhos

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './app/features/auth/contexts/AuthContext';
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <ToastContainer position="top-right" autoClose={3000} />
     </AuthProvider>
   </React.StrictMode>
 )
