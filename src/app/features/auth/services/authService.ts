05f119e567666f7cb26f935c89e54f8d4f0fae5c
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-05 16:58:19 -0300

    feat: integração com a api remota

--- src/app/features/auth/services/authService.ts
+++ src/app/features/auth/services/authService.ts
@@ -1,18 +1,17 @@
 import type { LoginPayload, LoginResponse, RegisterPayload } from '../types/auth.types'
 import api from '../../../../lib/api'
 
-
 export async function loginRequest(payload: LoginPayload): Promise<LoginResponse> {
-  const response = await api.post<LoginResponse>('/user/login', payload)
+  const response = await api.post<LoginResponse>('/user/login', payload, { withCredentials: true })
   return response.data
 }
 
 export async function registerUser(payload: RegisterPayload) {
-  const response = await api.post('/user/register', payload)
+  const response = await api.post('/user/register', payload, { withCredentials: true })
   return response.data
 }
 
 export const logoutService = async () => {
   // usa o mesmo cliente para limpar o cookie
-  await api.post('/user/logout')
+  await api.post('/user/logout', { withCredentials: true })
 }
